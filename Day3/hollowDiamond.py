428f3a170e124468542f9509a5bcfc9b75361073
Sanjana <sanjanaareddy0007@gmail.com>
2026-07-04 18:34:30 +0530

    HollowDiamond

---
+++ Day3/hollowDiamond.py
@@ -0,0 +1,15 @@
+
+n = int(input())
+for i in range(1,n+1):
+    spaces = (" ")*(n - i)
+    star = (" ")*i
+    print(space *(n - i) + star*i)
+
+for j in range(1, n- 1):
+    space = " "
+    star = ("* ")
+    print(" "*j + "*" + " "*k + "*")
+    k = k - 2
+  
+print(" "*(n - 1) + "*")
+
